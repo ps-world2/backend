0fcecf80e49d67951b5574a92501fb2b6c8d36ed
JeongGod <jeonggu223@gmail.com>
2022-06-08 17:42:11 +0900

    feat : Swagger API 연동

---
+++ src/main/java/com/pskjeong/psk/config/Swagger2Config.java
@@ -0,0 +1,27 @@
+package com.pskjeong.psk.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-psk")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("psk API")
+                        .description("psk 오픈 api 명세입니다.")
+                        .version("v0.0.1"));
+    }
+}
